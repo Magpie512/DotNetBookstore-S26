2dbfe43aea82a6ac4a676e369751b0ebc7a86564
Dario Hesami <dario.hesami@hotmail.com>
2026-05-14 19:58:03 -0400

    W01-Completed

---
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetBookstore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Browse(string category) {
+            ViewBag.Category = category;
+            
+            return View();
+        }
+    }
+}
